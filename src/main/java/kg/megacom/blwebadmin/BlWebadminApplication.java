d6aa79e1d14257303ffd88e7f55c14e955cea148
Namys <bnamys16@gmail.com>
2022-07-10 00:07:51 +0600

    init

---
+++ src/main/java/kg/megacom/blwebadmin/BlWebadminApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.blwebadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlWebadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlWebadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megacom/blwebadmin/BlWebadminApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.blwebadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlWebadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
